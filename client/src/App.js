7432b1ecc0e7b1a1cde852675fa8abb867208a42
Nathan LeRoy <NLeRoy917@gmail.com>
2021-01-26 14:16:17 -0500

    work on client styling + add logic to check for alreay existing urls

--- client/src/App.js
+++ client/src/App.js
@@ -6,35 +6,59 @@ import {CopyToClipboard} from 'react-copy-to-clipboard';
 const App = () => {
 
   const serverBase = window.location.href;
-  const [url, setUrl] = useState('')
-  const [shortUrl, setShortUrl] = useState('')
-  const [copyBtnText, setCopyBtnText] = useState('Copy')
+  const [url, setUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+  const [copyBtnText, setCopyBtnText] = useState('Copy');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const generateUrl = async () => {
-    setCopyBtnText('Copy')
-    setShortUrl("https://threeo.one/to/qw31rd")
+    /* 
+      Function to genreate a short-url. Checks to
+      make sure that thre is an input. Returns
+      if invalid. Then it tries to make a request
+      to the server to generate one. (See server
+      code for server logic). Catches and reports 
+      errors, otherwise displays results
+    */
     if (url === '') {
       alert('Please enter valid url')
+      return
     }
-    let res = await axios.get(`${serverBase}shorten`, { params: { url: url } } )
-    if (res.status === 200) {
-      let data = res.data
-      // console.log(data)
-      setShortUrl(data.shortUrl)
+    setLoading(true)
+    try {
+      let res = await axios.get(`${serverBase}shorten`, { params: { url: url } } )
+      if (res.status === 200) {
+        let data = res.data
+        // console.log(data)
+        setShortUrl(data.shortUrl)
+        setLoading(false)
+      }
+    } catch {
+      setError(true)
+      setLoading(false)
     }
   }
 
   return (
     <div className="layout">
       <div className="landing-container">
+      <div className="blob-top">
+        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+          <path fill="#FF5F5F" d="M23,-24.9C31.3,-7.9,40.6,1.7,39.3,9.7C38,17.6,26.2,23.8,13.2,31.6C0.2,39.4,-13.9,48.9,-31.2,47.3C-48.4,45.7,-68.6,33,-72.8,16.2C-76.9,-0.6,-65,-21.5,-50.2,-40C-35.4,-58.5,-17.7,-74.6,-5.2,-70.5C7.3,-66.4,14.7,-42,23,-24.9Z" transform="translate(100 100)" />
+        </svg>
+      </div>
         <h1 className="landing-title">threeo.one</h1>
         <p className="landing-body">Threeo.one (<em>"three-oh"</em> ) is a URL shortening service to turn even the longest of URL's into manageable and easily-sharable short-links. Simply enter your URL below and generate a short-link.</p>
-        <input
-          placeholder="Enter URL:"
-          className="url-input" 
-          type="text"
-          onChange={(e) => {setUrl(e.target.value)}}
-        />
+        <div className="input-loader-wrapper">
+          <input
+            placeholder="Enter URL:"
+            className="url-input" 
+            type="text"
+            onChange={(e) => {setUrl(e.target.value)}}
+          />
+          {loading ? <div class="lds-ring"><div></div><div></div><div></div><div></div></div> : <div></div>}
+        </div>
         <button
           className="generate-btn"
           onClick={()=>{generateUrl()}}
@@ -49,7 +73,12 @@ const App = () => {
                 <h4 className="short-url-title">Your short-url:</h4>
                 <a href={`${shortUrl}`}>{`${shortUrl}`}</a>
                 <CopyToClipboard text={shortUrl}
-                  onCopy={()=>{setCopyBtnText('Copied!')}}
+                  onCopy={()=>{
+                    setCopyBtnText('Copied!')
+                    setTimeout(() => {
+                      setCopyBtnText("Copy")
+                    }, 1500)
+                  }}
                 >
                   <button className="copy-btn">{`${copyBtnText}`}</button>
                 </CopyToClipboard>
@@ -57,6 +86,11 @@ const App = () => {
             : <div></div>
           }
         </div>
+      </div>
+      <div className="blob-bottom">
+      <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+        <path fill="#FF5F5F" d="M49.3,-69C62.5,-58.2,71,-42,74.8,-25.4C78.6,-8.9,77.8,8.1,70,19.9C62.2,31.6,47.4,38.3,34.6,46.7C21.7,55.1,10.9,65.3,-1.9,67.9C-14.7,70.5,-29.3,65.5,-39.1,56.1C-48.9,46.7,-53.9,32.9,-57.4,19.3C-60.9,5.7,-63,-7.7,-58,-17.5C-52.9,-27.2,-40.7,-33.2,-29.9,-44.8C-19,-56.3,-9.5,-73.4,4.3,-79.2C18,-85.1,36.1,-79.8,49.3,-69Z" transform="translate(100 100)" />
+      </svg>
       </div>
       <footer className="footer">
           <a className="link" href="https://github.com/nleroy917/threeo1">Source</a>
--- postgres.go
+++ postgres.go
@@ -9,6 +9,7 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// init the database - returns sql.DB struct pointer
 func initDatabase(file string) *sql.DB {
 	db, err := sql.Open("postgres", file)
 	if err != nil {
@@ -17,19 +18,67 @@ func initDatabase(file string) *sql.DB {
 	return db
 }
 
+// main function to shorten url's ----
+// first, the function checks if the url
+// exists in the database already...
+// 	if it does, it simply returns the short url
+// 	associated with that url.
+//	else it will create a new entry --
+//
+// To create a new entry, genreate a potential id,
+// while this id exists in the database, generate a
+// a new one. Once a unique id is found, then create
+// a new entry in the database
 func shortenUrl(url string, db *sql.DB) string {
 
 	var shortUriBase string = "https://threeo1.com/to/"
 	const ID_LENGTH = 6
 
+	// check if url already exists in db
+	// if it does return the short url for it
+	// shorturl == "" if url doesnt exist
+	shorturl := urlExists(url, db)
+	if len(shorturl) > 0 {
+		return shorturl
+	}
+
+	// genreate potential id
 	id := generateRandId(ID_LENGTH)
 
+	// while id exists in the database,
+	// generate a new one
 	for idExists(id, db) {
 		id = generateRandId(ID_LENGTH)
 	}
 
+	// insert the new entry
 	insertNewRedirect(url, id, db)
 
+	return (shortUriBase + id)
+}
+
+// check if a url exists - if it does, return short url,
+// else return an empty string
+func urlExists(potentialUrl string, db *sql.DB) string {
+
+	var id string
+	var url string
+	var shortUriBase string = "https://threeo1.com/to/"
+
+	// log.Println(potentialUrl)
+	row := db.QueryRow("SELECT id, redirectloc FROM URLS WHERE redirectloc= $1", potentialUrl)
+	err := row.Scan(&id, &url)
+
+	// will error out if the url is not found in the database
+	// thus, return empty string
+	if err != nil {
+		// log.Println("Not found")
+		return url
+	}
+
+	// else ...
+	// return the short id genreated from
+	// base + id
 	return (shortUriBase + id)
 }
 
@@ -53,7 +102,7 @@ func idExists(potentialId string, db *sql.DB) bool {
 func insertNewRedirect(url string, id string, db *sql.DB) error {
 	var err error = nil
 
-	_, dberr := db.Exec("INSERT INTO URLS(id, RedirectLoc) VALUES ($1, $2)", id, url)
+	_, dberr := db.Exec("INSERT INTO URLS(id, redirectloc) VALUES ($1, $2)", id, url)
 	if dberr != nil {
 		fmt.Println("Error")
 		log.Println(dberr.Error())
@@ -62,6 +111,8 @@ func insertNewRedirect(url string, id string, db *sql.DB) error {
 	return err
 }
 
+// function to create random alphanumeric
+// id's of length N.
 func generateRandId(N int) string {
 	char_map := [...]byte{'a', 'b', 'c', 'd',
 		'e', 'f', 'g', 'h', 'i',
